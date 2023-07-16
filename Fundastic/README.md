0c23c46714e5ac33b81ca3d641206713a4326ade
NotADev <127446095+ayushdev1@users.noreply.github.com>
2023-07-16 17:21:09 +0530

    Add files via upload
    files uploaded

---
+++ Fundastic/README.md
@@ -0,0 +1,20 @@
+# How to Run the Fundastic Platform for Users as well as Devlopers to create self made smart contract
+
+## For Users
+ Pre Dependencies- NPM package
+- Install npm while staying the main File directory on the terminal
+- cd into client by typing cd client in the terminal
+- Run  npm run dev 
+- Click on the link which appears
+### Pitch for Funds and Invest in other emerging startups!!!
+
+## For Creating a self made Smart contract
+ Pre Dependencies- NPM package
+- Install npm while staying the main File directory on the terminal
+- cd into web3 by typing cd web3 in the terminal
+- Run npm install --save-dev hardhat to install hardhat functionality locally
+- Run npm run deploy to deploy a smart contract
+- Proceed to the link of 'thirdweb' and copy your smart contract address
+- Paste the address into `client\src\context\index.jsx` in line 9 replacing the previous address
+- Fetch your MetaMask private key and paste it into `web3\.env`
+### Now all the deployed pitch of funds will be on your smart contract visible on ALL CAMPAIGN section of the page!!!
---
+++ Fundastic/client/src/context/index.jsx
@@ -0,0 +1,103 @@
+import React, { useContext, createContext } from 'react';
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract('0xCDCc2503AaBD69d515eD180eA5F921e2273baf7d');
+  const { mutateAsync: createCampaign,isLoading } = useContractWrite(contract, 'createCampaign');
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign(
+				[
+					address, 
+					form.title, 
+					form.description, 
+					form.target,
+					new Date(form.deadline).getTime(), 
+					form.image,
+          form.milestone,
+          form.reg,
+          new Date(form.miledeadline).getTime()
+				]
+			);
+
+      console.log("contract call success", data)
+    } catch (error) {
+      console.log("contract call failure", error)
+    }
+  }
+  
+  const getCampaigns = async () => {
+    const campaigns = await contract.call('getCampaigns');
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image: campaign.image,
+      pId: i
+    }));
+
+    return parsedCampaings;
+  }
+
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+
+    return filteredCampaigns;
+  }
+
+  const donate = async (pId, amount) => {
+    const data = await contract.call('donateToCampaign', [pId], { value: ethers.utils.parseEther(amount)});
+
+    return data;
+  }
+
+  const getDonations = async (pId) => {
+    const donations = await contract.call('getDonators', [pId]);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for(let i = 0; i < numberOfDonations; i++) {
+      parsedDonations.push({
+        donator: donations[0][i],
+        donation: ethers.utils.formatEther(donations[1][i].toString())
+      })
+    }
+
+    return parsedDonations;
+  }
+
+
+  return (
+    <StateContext.Provider
+      value={{ 
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+        getUserCampaigns,
+        donate,
+        getDonations
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(StateContext);
